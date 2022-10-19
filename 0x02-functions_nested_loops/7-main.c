613fcf13e45411399a98379ba49b8e6feefea96b
aziz300 <azeezfawaz80@yahoo.com>
2022-10-19 02:33:30 -0700

    Task 7

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -3,8 +3,9 @@
 /**
  * main - check the code
  *
- * Return - Always 0.
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	int r;
@@ -14,5 +15,7 @@ int main(void)
 	r = print_last_digit(-1024);
 	_putchar('0' + r);
 	_putchar('\n');
+
 	return (0);
+
 }
